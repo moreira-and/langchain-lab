c6053240e8e63a729594b932bd36e815f88b5ee7
André Moreira <89110440+moreira-and@users.noreply.github.com>
2026-04-05 23:07:50 +0000

    feat: add core processing nodes (uppercase, lowercase, intent, fallback, response)

---
+++ src/graph/nodes/lowerCaseNode.ts
@@ -0,0 +1,9 @@
+import { type GraphState } from "../graph.ts";
+
+export function lowerCaseNode(state: GraphState): GraphState {
+  const responseText = state.output.toLowerCase();
+  return {
+    ...state,
+    output:responseText
+  };
+}
